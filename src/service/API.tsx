1f9d706033aa6e0a919a2b8db97f5b54dc53f63b
tanmayrane18 <tanmayrane51@gmail.com>
2025-09-12 15:36:16 +0530

    app icon set

--- src/service/API.tsx
+++ src/service/API.tsx
@@ -1,4 +1,3 @@
-import { Platform } from 'react-native';
-
 export const GEMINI_API_KEY = 'AIzaSyCZFsr6-RDBvpdlUW1rtCX9EC80iNDvDmw';
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://192.168.0.104:3000';
+export const BASE_URL = 'https://baymax-fcm-server-llzp.onrender.com';
